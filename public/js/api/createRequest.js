177456d8d2655001913b544130b3d8e4404eaaf1
Yushkevich A.A. work computer <Mortifer1993@yandex.ru>
2021-03-23 14:55:31 +0300

    create createRequest

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,41 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    let dataUrl = options.url;
+    let keysOfData = Object.keys(options.data);    
+    let formData;
 
+    if (options.method === 'GET') {
+        dataUrl = options.url + '?';
+        const arr = [];
+        for (let key of keysOfData) {
+            arr.push(key + '=' + keysOfData[key]);
+        }
+        dataUrl = options.url + '?' + arr.join('&');
+    } else if (metod.data) {
+        formData = new FormData();
+        for (let key of keysOfData) {
+            formData.append(key, keysOfData[key]);
+        }     
+    }
+
+    const xhr = new XMLHttpRequest();  
+    xhr.open(options.method, dataUrl);    
+    xhr.responseType = options.responseType;
+    if (options.headers) {
+        let headers = Object.keys(options.headers);
+        for (let name of headers){
+            xhr.setRequestHeader(name, options.headers[name]);
+        }
+    }
+
+    xhr.addEventListener('readystatechange', () => {
+        if (xhr.readyState === xhr.DONE) {
+            options.callback(null, xhr.responseText);
+        } else {
+            options.callback(xhr.statusText);
+        }
+    })
+
+    (options.method === 'GET') ? xhr.send() : xhr.send(formData);
 };
